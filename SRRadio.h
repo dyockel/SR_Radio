8efff1e6e75acfc65646984f18a81688ac4ab4ab
tom jennings <tom@SensitiveResearch.com>
2015-05-09 17:25:22 -0700

    new
    combined SRnRF24L01+ driver and Flock protocol.

---
+++ SRRadio.h
@@ -0,0 +1,13 @@
+/*
+ 
+   unified SensitiveResearch radio protocol include file.
+
+   itself includes the files necessary to use the SRRF24
+   driver for the nRF24L01+ chip and the Flock and Peep
+   protocols.
+
+*/
+
+#include "SRRF24.h"
+#include "SRFlock.h"
+
